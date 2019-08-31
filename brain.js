d4ca5dab255a8b266a128d855442f45688aa3af8
Ishaan <ifarooq20@gmail.com>
2019-08-31 18:08:41 +0530

    Complete UI change
    Added some more fields and complete UI overhaul.

--- brain.js
+++ brain.js
@@ -11,7 +11,7 @@
                 const colors = response.outputs[0].data.colors;
                 colors.sort((a, b) => (a.value > b.value) ? -1 : ((b.value > a.value) ? 1 : 0));
 
-                const holder = document.querySelector('#colors');
+                const holder = document.querySelector('.color-list');
                 holder.innerHTML = '';
 
                 for (let i = 0; i < colors.length; i++) {
@@ -19,12 +19,21 @@
                     console.log(i)
 
                     // div for new pallete
-                    const addition = `
-                        <div id = "${i}"><span>
-                        ${color.raw_hex} </span><span>
-                        ${color.w3c.name} </span><span>
-                        ${color.value} </span></div>
-                    `;
+                    // const addition = `
+                    //     <div id = "${i}"><span>
+                    //     ${color.raw_hex} </span><span>
+                    //     ${color.w3c.name} </span><span>
+                    //     ${color.value} </span></div>
+                    // `;
+                    const addition = `<section class="color" id="${i}">
+                    <h2 class="name">${color.w3c.name}</h2>
+                    <ul class="details">
+                      <li>Raw HEX: ${color.raw_hex}</li>
+                      <!-- <li>${color.value}</li> -->
+                      <li>RGB: ${tinycolor(color.raw_hex).toRgbString()}</li>
+                      <li>HSL: ${tinycolor(color.raw_hex).toHslString()}</li>
+                    </ul>
+                  </section>`;
 
                     holder.innerHTML += addition;
 
